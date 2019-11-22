ace2b94218e38e7222c670f3d070a2fd2eda8566
FernandoRF <fernandoramiresfilho@gmail.com>
2019-11-22 20:34:15 -0300

    makefile changes

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Projeto_Odometro.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Projeto_Odometro.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projetoodometro.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projetoodometro.tar
